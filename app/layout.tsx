a2b65e579918c418f3f4befc180d26c6414dbc7e
squairdev <me@squair.xyz>
2026-02-13 16:19:26 -0500

    Complete rewrite in NextJS, updated steps and FAQ

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Pocket Poster Guide",
+  description: "A simple enough guide on how to install Pocket Poster on any iOS device. Pocket Poster only supports iOS 17-26.1.",
+  icons: {
+    icon: "favicon.ico",
+    shortcut: "favicon.ico",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://use.typekit.net" />
+        <link rel="stylesheet" href="https://use.typekit.net/taj5rlj.css" />
+      </head>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
